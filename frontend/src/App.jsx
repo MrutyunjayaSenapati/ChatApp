1256bd3cd4d92b730a4098408cf43d237cac24f4
Mrutyunjaya Senapati <mrutyunjayasenapati007@gmail.com>
2025-01-13 23:18:50 +0530

    Jan 13

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {Routes,Route} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+
+import ProfilePage from './pages/ProfilePage';
+import SettingPage from './pages/SettingPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <h1>Chat app</h1>
+  <Routes>
+
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/signup" element={<SignUpPage/>}/>
+    <Route path="/login" element={<LoginPage/>}/>
+    <Route path="/settings" element={<SettingPage/>}/>
+    <Route path="/profile" element={<ProfilePage/>}/>
+
+  </Routes>
+
+    </>
   )
 }
 
